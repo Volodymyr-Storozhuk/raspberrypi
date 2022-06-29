1f694a5f3b07c5059d38307d8527ce1928ef02eb
Volodymyr Storozhuk <42782158+Volodymyr-Storozhuk@users.noreply.github.com>
2022-06-29 10:52:33 +0300

    init button + LED code

---
+++ click_button/button+LED.py
@@ -0,0 +1,39 @@
+import time
+import sys
+import traceback
+import RPi.GPIO as GPIO
+
+# Секція ініціалізації змінних
+pinBtn = 24
+pinLED = 6
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pinBtn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(pinLED, GPIO.OUT, initial=0)
+
+# Секція функцій і класів defs, class
+if __name__ == '__main__':
+    try:
+        # якщо потрібно постійно виконувати код до преривання программи через Ctrl+C
+        while True:
+            statusPinBtn = GPIO.input(pinBtn)
+            print(statusPinBtn)
+            if statusPinBtn == 1:
+                GPIO.output(pinLED, GPIO.HIGH)
+            else:
+                GPIO.output(pinLED, GPIO.LOW)
+            time.sleep(0.5)
+
+    except KeyboardInterrupt:
+        # ...
+        print("Exit pressed Ctrl+C")
+    except Exception:
+        # ...
+        print("Other Exception")
+        print("--- Start Exception Data:")
+        traceback.print_exc(limit=2, file=sys.stdout)
+        print("--- End Exception Data:")
+    finally:
+        # ...
+        print("CleanUp")
+        GPIO.cleanup()
+        print("End of program")
---
+++ click_button/button.py
@@ -0,0 +1,33 @@
+import time
+import sys
+import traceback
+import RPi.GPIO as GPIO
+
+# Секція ініціалізації змінних
+pinBtn = 24
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pinBtn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Секція функцій і класів defs, class
+if __name__ == '__main__':
+    try:
+        # якщо потрібно постійно виконувати код до преривання программи через Ctrl+C
+        while True:
+            statusPinBtn = GPIO.input(pinBtn)
+            print(statusPinBtn)
+            time.sleep(0.5)
+
+    except KeyboardInterrupt:
+        # ...
+        print("Exit pressed Ctrl+C")
+    except Exception:
+        # ...
+        print("Other Exception")
+        print("--- Start Exception Data:")
+        traceback.print_exc(limit=2, file=sys.stdout)
+        print("--- End Exception Data:")
+    finally:
+        # ...
+        print("CleanUp")
+        GPIO.cleanup()
+        print("End of program")
