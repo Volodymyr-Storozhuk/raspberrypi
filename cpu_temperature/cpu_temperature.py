4f26664e9c580f6d05282fcb5b629fae26bc8f42
Volodymyr Storozhuk <42782158+Volodymyr-Storozhuk@users.noreply.github.com>
2022-08-29 12:37:51 +0300

    construction __main__

--- cpu_temperature/cpu_temperature.py
+++ cpu_temperature/cpu_temperature.py
@@ -9,25 +9,30 @@ from subprocess import check_output
 def get_temp():
     temp = check_output(["vcgencmd", "measure_temp"]).decode()
     temp = float(findall(r'\d+\.\d+', temp)[0])
-    return(temp)
+    return temp
 
 
-try:
-    while True:
-        temp = get_temp()
-        print('CPU temperature: ' + str(temp))
-        sleep(1)
-except KeyboardInterrupt:
-    # ...
-    print('Exit pressed Ctrl+C')
-except Exception:
-    # ...
-    # Прочие исключения
-    print("Other Exception")
-    print("--- Start Exception Data:")
-    # Подробности исключения через traceback
-    traceback.print_exc(limit=2, file=sys.stdout)
-    print("--- End Exception Data:")
-finally:
-    # ...
-    print('End program')
+def main():
+    try:
+        while True:
+            temp = get_temp()
+            print('CPU temperature: ' + str(temp))
+            sleep(1)
+    except KeyboardInterrupt:
+        # ...
+        print('Exit pressed Ctrl+C')
+    except Exception:
+        # ...
+        # Прочие исключения
+        print("Other Exception")
+        print("--- Start Exception Data:")
+        # Подробности исключения через traceback
+        traceback.print_exc(limit=2, file=sys.stdout)
+        print("--- End Exception Data:")
+    finally:
+        # ...
+        print('End program')
+
+
+if __name__ == '__main__':
+    main()
