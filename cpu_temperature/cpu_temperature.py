98d9fdae176226dcd3d360191968c6c48137c53f
volodymyr-storozhuk <volodymyr.storozhuk@hotmail.com>
2022-09-17 20:18:04 +0300

    add clear terminal info

--- cpu_temperature/cpu_temperature.py
+++ cpu_temperature/cpu_temperature.py
@@ -1,26 +1,30 @@
 import sys
 import traceback
+import os
 
 from time import sleep
 from re import findall
 from subprocess import check_output
 
 
-def get_temp():
-    temp = check_output(["vcgencmd", "measure_temp"]).decode()
-    temp = float(findall(r'\d+\.\d+', temp)[0])
-    return temp
+def get_cpu_temperature():
+    cpu_temperature = check_output(["vcgencmd", "measure_temp"]).decode()
+    cpu_temperature = float(findall(r'\d+\.\d+', cpu_temperature)[0])
+    return cpu_temperature
 
 
 def main():
     try:
         while True:
-            temp = get_temp()
-            print('CPU temperature: ' + str(temp))
-            sleep(1)
+            os.system('clear')
+            cpu_temperature = get_cpu_temperature()
+            print('CPU temperature: ' + str(cpu_temperature))
+            sleep(5)
+
     except KeyboardInterrupt:
         # ...
         print('Exit pressed Ctrl+C')
+
     except Exception:
         # ...
         # Прочие исключения
@@ -29,6 +33,7 @@ def main():
         # Подробности исключения через traceback
         traceback.print_exc(limit=2, file=sys.stdout)
         print("--- End Exception Data:")
+
     finally:
         # ...
         print('End program')
