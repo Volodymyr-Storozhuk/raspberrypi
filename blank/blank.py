12c5a3659c9a435ea71972853572408ab447c7c0
volodymyr-storozhuk <volodymyr.storozhuk@hotmail.com>
2022-09-17 20:13:02 +0300

    correct

--- blank/blank.py
+++ blank/blank.py
@@ -3,22 +3,30 @@ import traceback
 
 
 def test(message):
-    return(print(message))
+    return print(message)
 
 
-try:
-    test('Hello world!')
-except KeyboardInterrupt:
-    # ...
-    print('Exit pressed Ctrl+C')
-except Exception:
-    # ...
-    # Прочие исключения
-    print("Other Exception")
-    print("--- Start Exception Data:")
-    # Подробности исключения через traceback
-    traceback.print_exc(limit=2, file=sys.stdout)
-    print("--- End Exception Data:")
-finally:
-    # ...
-    print('End program')
+def main():
+    try:
+        test('Hello world!')
+
+    except KeyboardInterrupt:
+        # ...
+        print('Exit pressed Ctrl+C')
+
+    except Exception:
+        # ...
+        # Прочие исключения
+        print('Other exception')
+        print('--- Start Exception Data')
+        # Подробности исключения через traceback
+        traceback.print_exc(limit=2, file=sys.stdout)
+        print('--- End Exception Data')
+
+    finally:
+        # ...
+        print('End programm')
+
+
+if __name__ == '__main__':
+    main()
